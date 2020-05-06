8abafbc316cfd85f9222c4a8edfd88473a518639
larrythwu <larrythwu@gmail.com>
2020-05-06 00:27:49 -0400

    Completed all the sorting algorithms

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -21,7 +21,6 @@ let STOP = false;
 //indicate if a sorting is already running
 let RUNNING = false;
 
-
 //current array
 const arrayBars = document.getElementsByClassName('array-bar');
 
@@ -35,126 +34,161 @@ export default class SortingVisualizer extends React.Component {
   }
 
 //load for the first time, or refresh page
-  componentDidMount() {
-    this.resetArray();
-    STOP = false;
-  }
+componentDidMount() {
+  this.resetArray();
+  STOP = false;
+}
 
 //reset the array with new values, the same functio that is
-  resetArray() {
-    // console.log("Entered resetArray")
-    STOP = true;
-    RUNNING = false;
-    const array = [];
-    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-      // push number in the range of 10~500 into the array
-      array.push(randomIntFromInterval(10, 500));
-    }
-    //create new array, replace the previous one
-    this.setState({array});
+resetArray() {
+  // console.log("Entered resetArray")
+  STOP = true;
+  RUNNING = false;
+  const array = [];
+  for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+    // push number in the range of 10~500 into the array
+    array.push(randomIntFromInterval(10, 500));
+  }
+  //create new array, replace the previous one
+  this.setState({array});
+}
+
+async update_arrayBars(arr){
+  for(let i =0; i<arrayBars.length; i++){
+    arrayBars[i].style.height = `${arr[i]}px`;
+    if(STOP == false)
+    await new Promise(r => setTimeout(r,  ANIMATION_SPEED/5));
   }
 
-  mergeSort() {
-    // if(RUNNING == true)
-    //   return;
-    // else
-    //   RUNNING = true;
-    //
-    // const animations = getMergeSortAnimations(this.state.array);
-    // for (let i = 0; i < animations.length; i++) {
-    //   //const arrayBars = document.getElementsByClassName('array-bar');
-    //   const isColorChange = i % 3 !== 2; //either the first pair or the third pair
-    //   if (isColorChange) {
-    //     const [barOneIdx, barTwoIdx] = animations[i];
-    //     const barOneStyle = arrayBars[barOneIdx].style;
-    //     const barTwoStyle = arrayBars[barTwoIdx].style;
-    //     const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-    //
-    //     setTimeout(function(){
-    //       barOneStyle.backgroundColor = color;
-    //       barTwoStyle.backgroundColor = color;
-    //     }, i * ANIMATION_SPEED);
-    //   } else {
-    //     setTimeout(function(){
-    //       const [barOneIdx, newHeight] = animations[i];
-    //       const barOneStyle = arrayBars[barOneIdx].style;
-    //       barOneStyle.height = `${newHeight}px`;
-    //     }, i * ANIMATION_SPEED);
-    //
-    //     if(STOP == true){
-    //       STOP = false;
-    //       RUNNING = false;
-    //       return;
-    //     }
-    //   }
-    // }
+}
+
+async merge_sort(arr) {
+    let sorted = arr.slice();
+    let n = sorted.length;
+    let buffer = new Array(n);
+
+    for (let size = 1; size < n; size *= 2) {
+      for (let leftStart = 0; leftStart < n; leftStart += 2*size) {
+        let left = leftStart,
+            right = Math.min(left + size, n),
+            leftLimit = right,
+            rightLimit = Math.min(right + size, n),
+            i = left;
+        while (left < leftLimit && right < rightLimit) {
+          arrayBars[left].style.backgroundColor = SECONDARY_COLOR;
+          arrayBars[right].style.backgroundColor = SECONDARY_COLOR;
+          await new Promise(r => setTimeout(r,  ANIMATION_SPEED/5));
+          arrayBars[left].style.backgroundColor = PRIMARY_COLOR;
+          arrayBars[right].style.backgroundColor = PRIMARY_COLOR;
+
+          if(STOP == true){
+            await this.update_arrayBars(sorted);
+            STOP = false;
+            return sorted;
+          }
+
+          if (sorted[left] <= sorted[right]) {
+            buffer[i++] = sorted[left++];
+          } else {
+            buffer[i++] = sorted[right++];
+          }
+        }
+
+        while (left < leftLimit) {
+          arrayBars[left].style.backgroundColor = SECONDARY_COLOR;
+          await new Promise(r => setTimeout(r,  ANIMATION_SPEED/2));
+          arrayBars[left].style.backgroundColor = PRIMARY_COLOR;
+          buffer[i++] = sorted[left++];
+        }
+        while (right < rightLimit) {
+          arrayBars[right].style.backgroundColor = SECONDARY_COLOR;
+          await new Promise(r => setTimeout(r,  ANIMATION_SPEED/2));
+          arrayBars[right].style.backgroundColor = PRIMARY_COLOR;
+          buffer[i++] = sorted[right++];
+        }
+      }
+      await this.update_arrayBars(buffer);
+
+      let temp = sorted;
+          sorted = buffer;
+          buffer = temp;
+    }
+    return sorted;
+  }
+
+  async mergeSort() {
+    if(RUNNING == true)
+      return;
+    else
+      RUNNING = true;
+    STOP = false;
+
+    this.state.array = await this.merge_sort(this.state.array);
+    RUNNING = false;
   }
 
  swap (arr, i, j){
       let tmp = arr[i];
       arr[i] = arr[j];
       arr[j] = tmp;
-
-      //let arrayBars = document.getElementsByClassName('array-bar');
       arrayBars[i].style.height = `${arr[i]}px`;
       arrayBars[j].style.height = `${arr[j]}px`;
   }
 
-   async partition (arr, low, high){
-      let q = low;
-      let i = 0;
-      arrayBars[high].style.backgroundColor = 'green';
+ async partition (arr, low, high){
+    let q = low;
+    let i = 0;
+    arrayBars[high].style.backgroundColor = 'green';
 
-      for (i = low; i < high; i++) {
-        arrayBars[i].style.backgroundColor = SECONDARY_COLOR;
-        arrayBars[q].style.backgroundColor = 'blue';
-        if (arr[i] < arr[high]) {
-            await new Promise(r => setTimeout(r,  ANIMATION_SPEED/10));
-            arrayBars[q].style.backgroundColor = PRIMARY_COLOR;
-            this.swap(arr, i, q);
-            q++;
-        }
-        await new Promise(r => setTimeout(r,  ANIMATION_SPEED/10));
-        arrayBars[i].style.backgroundColor = PRIMARY_COLOR;
-
-        if(STOP == true){
-          arrayBars[high].style.backgroundColor = PRIMARY_COLOR;
-          arrayBars[i].style.backgroundColor = PRIMARY_COLOR;
+    for (i = low; i < high; i++) {
+      arrayBars[i].style.backgroundColor = SECONDARY_COLOR;
+      arrayBars[q].style.backgroundColor = 'blue';
+      if (arr[i] < arr[high]) {
+          await new Promise(r => setTimeout(r,  ANIMATION_SPEED/10));
           arrayBars[q].style.backgroundColor = PRIMARY_COLOR;
-          return;
-        }
+          this.swap(arr, i, q);
+          q++;
       }
-      arrayBars[high].style.backgroundColor = PRIMARY_COLOR;
-      arrayBars[q].style.backgroundColor = PRIMARY_COLOR;
+      await new Promise(r => setTimeout(r,  ANIMATION_SPEED/10));
+      arrayBars[i].style.backgroundColor = PRIMARY_COLOR;
 
-      this.swap(arr, i, q);
-      return q;
+      if(STOP == true){
+        arrayBars[high].style.backgroundColor = PRIMARY_COLOR;
+        arrayBars[i].style.backgroundColor = PRIMARY_COLOR;
+        arrayBars[q].style.backgroundColor = PRIMARY_COLOR;
+        return;
+      }
     }
+    arrayBars[high].style.backgroundColor = PRIMARY_COLOR;
+    arrayBars[q].style.backgroundColor = PRIMARY_COLOR;
 
-  async quickSortHelper(arr, low, high){
-      if (low < high) {
-          let pivot = await this.partition(arr, low, high);
-
-          //const arrayBars = document.getElementsByClassName('array-bar');
-          //arrayBars[pivot].style.backgroundColor = SECONDARY_COLOR;
-          //await new Promise(r => setTimeout(r,  ANIMATION_SPEED*10));
-          //arrayBars[pivot].style.backgroundColor = PRIMARY_COLOR;
-          if(STOP == true){
-            RUNNING = false;
-            return;
-          }
-          await this.quickSortHelper(arr, low, pivot - 1);
-          await this.quickSortHelper(arr, pivot + 1, high);
-          return arr;
-      }
+    this.swap(arr, i, q);
+    return q;
   }
 
-  async quickSort() {
+  async quickSortHelper(arr, low, high){
+    if (low < high) {
+        let pivot = await this.partition(arr, low, high);
+
+        //const arrayBars = document.getElementsByClassName('array-bar');
+        //arrayBars[pivot].style.backgroundColor = SECONDARY_COLOR;
+        //await new Promise(r => setTimeout(r,  ANIMATION_SPEED*10));
+        //arrayBars[pivot].style.backgroundColor = PRIMARY_COLOR;
+        if(STOP == true){
+          // RUNNING = false;
+          return;
+        }
+        await this.quickSortHelper(arr, low, pivot - 1);
+        await this.quickSortHelper(arr, pivot + 1, high);
+        return arr;
+    }
+  }
+
+async quickSort() {
     if(RUNNING == true)
       return;
     else
       RUNNING = true;
-
     STOP = false;
 
 
@@ -164,14 +198,93 @@ export default class SortingVisualizer extends React.Component {
 
     STOP = false;
     RUNNING = false;
+}
+
+async heapify(heap, i, max) {
+    var index, leftChild, righChild;
+
+    while(i < max) {
+      index = i;
+
+      leftChild = 2*i + 1;
+      righChild = leftChild + 1;
+
+      if (leftChild < max && heap[leftChild] > heap[index]) {
+        index = leftChild;
+      }
+
+      if (righChild < max && heap[righChild] > heap[index]) {
+        index = righChild;
+      }
+
+      if (index == i) {
+        return;
+      }
+
+      arrayBars[i].style.backgroundColor = SECONDARY_COLOR;
+      arrayBars[index].style.backgroundColor = SECONDARY_COLOR;
+      await new Promise(r => setTimeout(r,  ANIMATION_SPEED/2));
+      arrayBars[i].style.backgroundColor = PRIMARY_COLOR;
+      arrayBars[index].style.backgroundColor = PRIMARY_COLOR;
+      this.swap(heap,i, index);
+
+      i = index;
+
+      if(STOP == true)
+        return;
+
   }
+}
 
-  heapSort() {
+async buildMaxHeap(array) {
+    var i;
+    i = array.length / 2 - 1;
+    i = Math.floor(i);
 
-  }
+    // Build a max heap out of
+    // all array elements passed in.
+    while (i >= 0) {
+      await this.heapify(array, i, array.length);
+      i -= 1;
+      if(STOP == true)
+        return;
+    }
+}
+
+async heapSort() {
+    if(RUNNING == true)
+      return;
+    else
+      RUNNING = true;
+    STOP = false;
 
 
-  async bubbleSort() {
+    let array = this.state.array;
+    // Build our max heap.
+    await this.buildMaxHeap(array);
+
+    // Find last element.
+    let lastElement = array.length - 1;
+
+    // Continue heap sorting until we have
+    // just one element left in the array.
+    while(lastElement > 0) {
+      this.swap(array, 0, lastElement);
+
+      await this.heapify(array, 0, lastElement);
+
+      lastElement -= 1
+
+      if(STOP == true)
+        break;
+    }
+
+    STOP = false;
+    RUNNING = false;
+}
+
+
+async bubbleSort() {
     if(RUNNING == true)
       return;
     else
@@ -187,7 +300,7 @@ export default class SortingVisualizer extends React.Component {
           //let arrayBars = document.getElementsByClassName('array-bar');
           arrayBars[j].style.backgroundColor = SECONDARY_COLOR;
           arrayBars[j+1].style.backgroundColor = SECONDARY_COLOR;
-          await new Promise(r => setTimeout(r,  ANIMATION_SPEED));
+          await new Promise(r => setTimeout(r,  ANIMATION_SPEED/30));
 
           if(array[j] > array[j+1]){
             sorted = false;
@@ -199,7 +312,7 @@ export default class SortingVisualizer extends React.Component {
           }
           arrayBars[j].style.backgroundColor = PRIMARY_COLOR;
           arrayBars[j+1].style.backgroundColor = PRIMARY_COLOR;
-          await new Promise(r => setTimeout(r,  ANIMATION_SPEED/5));
+          await new Promise(r => setTimeout(r,  ANIMATION_SPEED/30));
 
         if(STOP == true){
           STOP = false;
@@ -212,59 +325,55 @@ export default class SortingVisualizer extends React.Component {
         }
     }
     RUNNING = false;
-  }
+}
 
-  stop(){
-    // console.log("Entered stop toggle")
-    STOP = !STOP;
-  }
-
+stop(){
+  STOP = !STOP;
+}
 
 
-  render() {
-    const {array} = this.state;
+render() {
+  const {array} = this.state;
 
-    return (
-      <div className="array-container">
-      <div className = "button-container">
-        <Button onClick={() => this.resetArray()} type="button"
-        buttonStyle="btn--success--solid"
-        buttonSize="btn--medium">Generate New Array</Button>
+  return (
+    <div className="array-container">
+    <div className = "button-container">
+      <Button onClick={() => this.resetArray()} type="button"
+      buttonStyle="btn--success--solid"
+      buttonSize="btn--medium">Generate New Array</Button>
 
-        <Button onClick={() => this.mergeSort()} type="button"
-        buttonStyle="btn--primary--outline"
-        buttonSize="btn--medium">Merge Sort</Button>
+      <Button onClick={() => this.mergeSort()} type="button"
+      buttonStyle="btn--primary--outline"
+      buttonSize="btn--medium">Merge Sort</Button>
 
-        <Button onClick={() => this.quickSort()} type="button"
-        buttonStyle="btn--primary--outline"
-        buttonSize="btn--medium">Quick Sort</Button>
+      <Button onClick={() => this.quickSort()} type="button"
+      buttonStyle="btn--primary--outline"
+      buttonSize="btn--medium">Quick Sort</Button>
 
-        <Button onClick={() => this.heapSort()} type="button"
-        buttonStyle="btn--primary--outline"
-        buttonSize="btn--medium">Heap Sort</Button>
+      <Button onClick={() => this.heapSort()} type="button"
+      buttonStyle="btn--primary--outline"
+      buttonSize="btn--medium">Heap Sort</Button>
 
-        <Button onClick={() => this.bubbleSort()} type="button"
-        buttonStyle="btn--primary--outline"
-        buttonSize="btn--medium">Bubble Sort</Button>
+      <Button onClick={() => this.bubbleSort()} type="button"
+      buttonStyle="btn--primary--outline"
+      buttonSize="btn--medium">Bubble Sort</Button>
 
-        <Button onClick={() => this.stop()} type="button"
-        buttonStyle="btn--danger--solid"
-        buttonSize="btn--medium">Stop</Button>
-        </div>
-        
-        {array.map((value, idx) => (
-          <div
-            className="array-bar"
-            key={idx}
-            style={{
-              backgroundColor: PRIMARY_COLOR,
-              height: `${value}px`,
-            }}></div>
-        ))}
-
-
-
+      <Button onClick={() => this.stop()} type="button"
+      buttonStyle="btn--danger--solid"
+      buttonSize="btn--medium">Stop</Button>
       </div>
+
+      {array.map((value, idx) => (
+        <div
+          className="array-bar"
+          key={idx}
+          style={{
+            backgroundColor: PRIMARY_COLOR,
+            height: `${value}px`,
+          }}></div>
+      ))}
+
+    </div>
     );
   }
 }
@@ -273,14 +382,4 @@ export default class SortingVisualizer extends React.Component {
 function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function arraysAreEqual(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) return false;
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
 }
